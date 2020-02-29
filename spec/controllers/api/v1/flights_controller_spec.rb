284f5235ccdc69c2c851617d1e0e245e0f2aa16d
Antonio Ganci <aganci@gmail.com>
2020-02-29 15:48:09 +0100

    add spec for unauthorized api request

--- spec/controllers/api/v1/flights_controller_spec.rb
+++ spec/controllers/api/v1/flights_controller_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::FlightsController do
   describe 'GET api/v1/flights' do
-    before do
-      user = create(:user)
-      command = AuthenticateUser.new(user.email, user.password).call
-      request.headers.merge!({'Authorization': "Bearer #{command.result}"})
+    context 'not authenticated' do
+      it 'should list flights' do
+        get :find, format: :json
+
+        expect(response.status).to be(401)
+      end
     end
 
-    it 'should list flights' do
-      get :find, format: :json
+    context 'authenticated' do
+      before do
+        user = create(:user)
+        command = AuthenticateUser.new(user.email, user.password).call
+        request.headers.merge!({'Authorization': "Bearer #{command.result}"})
+      end
 
-      expect(response).to be_successful
+      it 'should list flights' do
+        get :find, format: :json
+
+        expect(response).to be_successful
+      end
     end
   end
 end
